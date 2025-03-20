26e349525ff3f50dc05d10c7a7549275e0441910
Hernan Pereyra <hernan.pereyra@alsacia.com.uy>
2025-03-20 18:08:31 -0300

    linting

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ const PORT = process.env.PORT || 3000;
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  //console.log(`Server is running on http://localhost:${PORT}`);
 });
