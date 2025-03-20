0b9da443778e35e831d8a3b2ee5dc72ccc9f85db
Hernan Pereyra <hernan.pereyra@alsacia.com.uy>
2025-03-20 18:01:31 -0300

    linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,44 @@
-/** @type {import('eslint').Linter.Config} */
-const eslintConfig = [
+// /** @type {import('eslint').Linter.Config} */
+// const eslintConfig = [
+//   {
+//     languageOptions: {
+//       ecmaVersion: 2021,
+//       sourceType: "module",
+//       globals: {
+//         process: 'readonly',  // Define Node.js global variables
+//         __dirname: 'readonly',
+//       },
+//     },
+//     plugins: {
+//       node: require("eslint-plugin-node"), // Load Node plugin
+//       prettier: require("eslint-plugin-prettier"), // Load Prettier plugin
+//     },
+//     env: {
+//       node: true,  // This will set Node.js-specific globals
+//     },
+//     rules: {
+//       "no-console": "off", //"warn",
+//       "prettier/prettier": "error", // Prettier rules for code style
+//     },
+//   },
+// ];
+
+// module.exports = eslintConfig;
+
+// eslint.config.js
+
+// import { FlatConfigArray } from '@eslint/config-array';
+
+export default [
   {
     languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
-    plugins: {
-      node: require("eslint-plugin-node"), // Load Node plugin
-      prettier: require("eslint-plugin-prettier"), // Load Prettier plugin
+      globals: {
+        process: 'readonly',  // Node.js global variable
+        __dirname: 'readonly', // Node.js global variable
+      },
     },
     rules: {
-      "no-console": "off", //"warn",
-      "prettier/prettier": "error", // Prettier rules for code style
+      'no-console': 'warn',  // Example rule to allow console logs
     },
   },
 ];
-
-module.exports = eslintConfig;
